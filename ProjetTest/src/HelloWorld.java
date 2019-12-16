755cd9e316dcf6805d080062ccc412d638fdb8b5
titibrown <58141406+titibrown@users.noreply.github.com>
2019-12-16 16:56:17 +0100

    Suite

--- ProjetTest/src/HelloWorld.java
+++ ProjetTest/src/HelloWorld.java
@@ -11,6 +11,9 @@ public class HelloWorld {
 	/**
 	 * @param args
 	 */
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("la vie est belle");
