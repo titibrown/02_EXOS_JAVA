f52e9b1b5035a95fe9a929c589a8372da1ab1268
titibrown <58141406+titibrown@users.noreply.github.com>
2019-12-17 10:54:43 +0100

    Commit suite

--- ProjetTest/src/HelloWorld.java
+++ ProjetTest/src/HelloWorld.java
@@ -17,6 +17,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println ("la vie est belle");
-		System.out.println ("Mais difficile"); 
+		System.out.println ("Mais difficile");
+		System.out.println ("La baise");
 }
 }
