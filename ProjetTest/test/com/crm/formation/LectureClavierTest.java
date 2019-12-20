53f4a1597103320582d7d9c6734196f4b14be348
titibrown <58141406+titibrown@users.noreply.github.com>
2019-12-20 09:50:25 +0100

    Suite

--- ProjetTest/test/com/crm/formation/LectureClavierTest.java
+++ ProjetTest/test/com/crm/formation/LectureClavierTest.java
@@ -15,10 +15,17 @@ public class LectureClavierTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Entrer un mot");
-		String str = sc.nextLine();
-		System.out.println("Vous avez saisi"+ str);
+		String str;
+		str = sc.nextLine();
+		System.out.println("Vous avez saisi "+ str);
+		System.out.println("\nVoulez vous afficher un message de fin ? (oui/non)");
+		str = sc.nextLine();
+		System.out.println("Vous avez saisi "+ str);
+		if (str == "oui") {
+			System.out.println("FIN");
+		}
 	}
 }
