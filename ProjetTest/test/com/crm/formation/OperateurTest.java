62201621942693bcd476418687986cd225b3b013
titibrown <58141406+titibrown@users.noreply.github.com>
2020-01-03 10:05:11 +0100

    fIRST

---
+++ ProjetTest/test/com/crm/formation/OperateurTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+import java.util.Scanner;
+
+/**
+ * @author CRM
+ *
+ */
+public class OperateurTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int maVariableInt1;
+		maVariableInt1 = 36;
+		System.out.println(maVariableInt1);
+		
+		int maVariableInt2;
+		maVariableInt2 = maVariableInt1;
+		System.out.println(maVariableInt2);
+		
+		int maVariableInt3;
+		maVariableInt3 = maVariableInt2 = maVariableInt1 = 58;
+		System.out.println(maVariableInt1);
+		System.out.println(maVariableInt2);
+		System.out.println(maVariableInt3);
+		
+		
+		
+	}
+
+}
