347df0e724bc4fa2d48d4e994f0e341550473707
titibrown <58141406+titibrown@users.noreply.github.com>
2020-01-16 15:59:57 +0100

    suite

---
+++ projet2/src/projet2/Math.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package projet2;
+
+/**
+ * @author CRM
+ *
+ */
+	public class Math {
+	public static int Math(int a, int b) {
+		int s = a - b;
+		return s;
+		
+		
+		
+		
+		
+}
+}
